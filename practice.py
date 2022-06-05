a80e7c9eca60bad92ba5bd45130fdfd242cdb660
idoneve <39467668+idoneve@users.noreply.github.com>
2022-06-05 16:49:14 -0600

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+
+def is_prime(n):
+    if n < 2: return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0: return False
+    return True
+
+primes, x = [i for i in range(0, 10000) if is_prime(i)], []
+def animate(i):
+    x.append(primes[i])
+    ax.clear()
+    plt.polar(x, 'o', markersize=5)
+
+anim = animation.FuncAnimation(fig, animate, frames=len(primes), interval=5, repeat=False)
+
+plt.grid()
+plt.show()
